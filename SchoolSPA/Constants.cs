881c9a969d52e34b4640bdec3ecf8d6c41864ab7
Shalana <shalana@Shalanas-MacBook-Pro.local>
2024-03-09 23:11:32 -0800

    4

--- SchoolSPA/Constants.cs
+++ SchoolSPA/Constants.cs
@@ -2,7 +2,7 @@
 
 public class Constants
 {
-    public const string SchoolAPIBase = "/api/";
+    //public const string SchoolAPIBase = "/api/";
     //public const string SchoolAPIBase = "http://localhost:5208/api/";
-    //public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
+    public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
 }
